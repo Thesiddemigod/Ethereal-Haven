c0ed2bb115160b09a504de093019fe7b36f03d07
Thesiddemigod <153060675+Thesiddemigod@users.noreply.github.com>
2023-12-25 14:37:49 +0530

    Create slider.php

---
+++ EtherealHaven/app/pages/includes/slider.php
@@ -0,0 +1,25 @@
+<!-- slider -->
+<link rel="stylesheet" href="<?= ROOT ?>/assets/slider/ism/css/my-slider.css" />
+<script src="<?= ROOT ?>/assets/slider/ism/js/ism-2.2.min.js"></script>
+
+<div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
+  <ol>
+    <li>
+      <img src="<?= ROOT ?>/assets/slider/ism/image/slides/whsi1.jpeg">
+      <div class="ism-caption ism-caption-0">"Unleash Wonder.</div>
+    </li>
+    <li>
+      <img src="<?= ROOT ?>/assets/slider/ism/image/slides/whsi2.jpeg">
+      <div class="ism-caption ism-caption-0">Explore Narratives, Insights,</div>
+    </li>
+    <li>
+      <img src="<?= ROOT ?>/assets/slider/ism/image/slides/whsi3.jpeg">
+      <div class="ism-caption ism-caption-0">and Mastery from Writers Across Every Realm.</div>
+    </li>
+    <li>
+      <img src="<?= ROOT ?>/assets/slider/ism/image/slides/whsi4.jpeg">
+      <div class="ism-caption ism-caption-0">EtherealHaven: Your Gateway to Infinite Curiosity."</div>
+    </li>
+  </ol>
+</div>
+<!-- end slider -->
