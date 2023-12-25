c3d85500d012056d6df92f610ebac670341b5454
Thesiddemigod <153060675+Thesiddemigod@users.noreply.github.com>
2023-12-25 14:35:32 +0530

    Create about.php

---
+++ EtherealHaven/app/pages/about.php
@@ -0,0 +1,87 @@
+<?php include '../app/pages/includes/header.php'; ?>
+
+<div class="about-us-container" style="position: relative;">
+    <div class="background-overlay"
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: url('assets/images/bgau.jpeg') center / cover; opacity: 0.55; z-index: 0;">
+    </div>
+
+    <div class="container mt-3 mb-3 text-center" style="color: #000080; font-size: 16px; position: relative; z-index: 1;">
+        <h1 class="display-3" style="text-decoration: underline; font-weight: bold;">Welcome to EtherealHaven</h1>
+
+        <p class="mx-4" style="font-weight: bold; font-size: 1.2em;">
+            <i>At EtherealHaven, we believe in the power of stories to inspire, connect, and enrich lives. In this
+                digital
+                sanctuary, we invite you to embark on a journey of exploration and enlightenment. Our haven is a place
+                where
+                diverse voices converge, and each story is a thread weaving the tapestry of shared human
+                experiences.</i>
+        </p>
+
+        <h2 class="mx-4 mt-4" style="font-weight: bolder; font-size: 2em;">Our Purpose</h2>
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>Everyone has a story, and EtherealHaven is dedicated to providing a platform for those unique narratives.
+                We
+                are more than just a blog; we are a haven for genuine expression, where anyone can contribute insightful
+                perspectives, share valuable knowledge, and impart life wisdom with the world. No need to build a
+                massive
+                following or a mailing list—here, your voice resonates organically.</i>
+        </p>
+
+        <h2 class="mx-4 mt-4" style="font-weight: bolder; font-size: 2em;">Navigating the Chaos</h2>
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>In the midst of the internet's noisy and chaotic landscape, EtherealHaven stands as a serene oasis. We
+                prioritize simplicity, beauty, and collaboration. Our platform is designed to cut through the clutter,
+                offering a quiet space filled with profound insights. It’s a place where substance prevails over
+                superficiality, and thoughtful conversations thrive.</i>
+        </p>
+
+        <h2 class="mx-4 mt-4" style="font-weight: bolder; font-size: 2em;">Words that Matter</h2>
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>We believe in the profound impact of words. Words have the power to divide or empower, inspire or
+                discourage. In a world where sensationalism often takes the spotlight, we are committed to building a
+                system
+                that rewards depth, nuance, and time well spent. EtherealHaven is more than just a collection of
+                articles;
+                it's a celebration of meaningful storytelling.</i>
+        </p>
+
+        <h2 class="mx-4 mt-4" style="font-weight: bolder; font-size: 2em;">Our Collective Journey</h2>
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>Ultimately, our goal is to deepen our collective understanding of the world through the transformative
+                power
+                of writing. EtherealHaven is a community where writers and readers alike contribute to a shared tapestry
+                of
+                insights, perspectives, and knowledge.</i>
+        </p>
+
+        <h2 class="mx-4 mt-4" style="font-weight: bolder; font-size: 2em;">Join Our Haven</h2>
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>Over countless stories, EtherealHaven has become a gathering place for over many people every month.
+                Our contributors range from professional writers to CEOs, scientists, creatives, and everyday
+                individuals
+                burning with stories to share. Here, you'll find reflections on work, midnight musings, life
+                experiences,
+                and lessons learned.</i>
+        </p>
+
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>Instead of relying on ads or selling your data, EtherealHaven thrives with the support of a growing
+                community of members who resonate with our mission. If you're new here, we invite you to explore and
+                dive
+                deeper into whatever matters to you. Discover posts that help you learn something new, challenge your
+                perspectives, and inspire you. And when you're ready, share your own story with our community.</i>
+        </p>
+
+        <p class="mx-4" style="font-weight: bold; font-size: 1.1em;">
+            <i>Welcome to EtherealHaven—where every story is a piece of a larger, collective narrative.</i>
+        </p>
+        <?php for ($i = 0; $i < 3; $i++): ?>
+            <p class="mx-4">&nbsp;</p>
+        <?php endfor; ?>
+
+        <?php for ($i = 0; $i < 10; $i++): ?>
+            <p class="mx-4">&nbsp;</p>
+        <?php endfor; ?>
+    </div>
+</div>
+<?php include '../app/pages/includes/footer.php'; ?>
