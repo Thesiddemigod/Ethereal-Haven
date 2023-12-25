1b4f587cae0ec5b742732b69e087238d0b02a2e8
Thesiddemigod <153060675+Thesiddemigod@users.noreply.github.com>
2023-12-25 14:36:36 +0530

    Create contact.php

---
+++ EtherealHaven/app/pages/contact.php
@@ -0,0 +1,7 @@
+<?php include '../app/pages/includes/header.php'; ?>
+
+<center>
+   <h4>Contact details:</h4>
+   Email: "Your.email@gmail.com"
+</center>
+<?php include '../app/pages/includes/footer.php'; ?>
